0198efb968ed1d9d43e132282e858e7c6283be39
M IQBAL AR <32996401+kingrootid@users.noreply.github.com>
2021-10-01 15:53:48 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,33 +5,6 @@ import VueRouter from "vue-router";
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
-
-// ----------> Custom Directives <-----------
-// Vue.directive("rainbow", {
-//   bind(el) {
-//     el.style.color = "red";
-//   },
-// });
-
-/*Vue.directive("theme", {
-  bind(el, binding) {
-    if (binding.value === "wide") {
-      el.style.maxWidth = "1200px";
-    } else if (binding.value === "narrow") {
-      el.style.maxWidth = "560px";
-    }
-    if (binding.arg == "column") {
-      el.style.background = "#ddd";
-      el.style.padding = "20px";
-    }
-  },
-});*/
-
-// -----------> Filters <--------------
-// Vue.filter("snippet", (value) => {
-//   return value.slice(0, 100) + "...";
-// });
-
 // -----------> Router <---------------
 import Routes from "./routes";
 const router = new VueRouter({
